cadeed13515cb662d33a38483bd6fef954a65131
khmilevoi <Khmilevoi@gmail.com>
2025-08-14 15:27:11 +0200

    Run coverage check in pre-commit hook

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unused-modules */
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      thresholds: { lines: 90, functions: 90, branches: 90 },
+    },
+  },
+});
