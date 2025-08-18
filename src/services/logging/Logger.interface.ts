836079dccc0106b59742d3c5c7a1052f679f9031
khmilevoi <Khmilevoi@gmail.com>
2025-08-18 22:00:37 +0200

    feat(logging): add logger interface

---
+++ src/services/logging/Logger.interface.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-unused-modules */
+interface Logger {
+  debug(message: string, meta?: Record<string, unknown>): void;
+  info(message: string, meta?: Record<string, unknown>): void;
+  warn(message: string, meta?: Record<string, unknown>): void;
+  error(message: string, meta?: Record<string, unknown>): void;
+  child(meta: Record<string, unknown>): Logger;
+}
+
+export default Logger;
