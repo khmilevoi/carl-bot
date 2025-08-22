9e226f110b81a6154532e8c73ebc359a27a702fa
khmilevoi <Khmilevoi@gmail.com>
2025-08-22 03:47:09 +0200

    chore: bind interfaces to implementations

--- src/container.ts
+++ src/container.ts
@@ -2,21 +2,42 @@ import 'reflect-metadata';
 
 import { Container } from 'inversify';
 
-import { ADMIN_SERVICE_ID } from './application/interfaces/admin/AdminService.interface';
-import { AI_SERVICE_ID } from './application/interfaces/ai/AIService.interface';
-import { CHAT_APPROVAL_SERVICE_ID } from './application/interfaces/chat/ChatApprovalService.interface';
+import {
+  ADMIN_SERVICE_ID,
+  type AdminService,
+} from './application/interfaces/admin/AdminService.interface';
+import {
+  AI_SERVICE_ID,
+  type AIService,
+} from './application/interfaces/ai/AIService.interface';
+import {
+  CHAT_APPROVAL_SERVICE_ID,
+  type ChatApprovalService,
+} from './application/interfaces/chat/ChatApprovalService.interface';
 import {
   CHAT_CONFIG_SERVICE_ID,
   type ChatConfigService,
 } from './application/interfaces/chat/ChatConfigService.interface';
-import { CHAT_MEMORY_MANAGER_ID } from './application/interfaces/chat/ChatMemoryManager.interface';
-import { CHAT_RESET_SERVICE_ID } from './application/interfaces/chat/ChatResetService.interface';
+import {
+  CHAT_MEMORY_MANAGER_ID,
+  type ChatMemoryManager as ChatMemoryManagerInterface,
+} from './application/interfaces/chat/ChatMemoryManager.interface';
+import {
+  CHAT_RESET_SERVICE_ID,
+  type ChatResetService,
+} from './application/interfaces/chat/ChatResetService.interface';
 import {
   CHAT_RESPONDER_ID,
   type ChatResponder,
 } from './application/interfaces/chat/ChatResponder.interface';
-import { DIALOGUE_MANAGER_ID } from './application/interfaces/chat/DialogueManager.interface';
-import { HISTORY_SUMMARIZER_ID } from './application/interfaces/chat/HistorySummarizer.interface';
+import {
+  DIALOGUE_MANAGER_ID,
+  type DialogueManager,
+} from './application/interfaces/chat/DialogueManager.interface';
+import {
+  HISTORY_SUMMARIZER_ID,
+  type HistorySummarizer,
+} from './application/interfaces/chat/HistorySummarizer.interface';
 import {
   TRIGGER_PIPELINE_ID,
   type TriggerPipeline,
@@ -25,19 +46,37 @@ import {
   ENV_SERVICE_ID,
   type EnvService,
 } from './application/interfaces/env/EnvService.interface';
-import { INTEREST_CHECKER_ID } from './application/interfaces/interest/InterestChecker.interface';
-import { LOGGER_FACTORY_ID } from './application/interfaces/logging/LoggerFactory.interface';
-import { INTEREST_MESSAGE_STORE_ID } from './application/interfaces/messages/InterestMessageStore.interface';
+import {
+  INTEREST_CHECKER_ID,
+  type InterestChecker,
+} from './application/interfaces/interest/InterestChecker.interface';
+import {
+  LOGGER_FACTORY_ID,
+  type LoggerFactory,
+} from './application/interfaces/logging/LoggerFactory.interface';
+import {
+  INTEREST_MESSAGE_STORE_ID,
+  type InterestMessageStore,
+} from './application/interfaces/messages/InterestMessageStore.interface';
 import {
   MESSAGE_CONTEXT_EXTRACTOR_ID,
   type MessageContextExtractor,
 } from './application/interfaces/messages/MessageContextExtractor.interface';
-import { MESSAGE_SERVICE_ID } from './application/interfaces/messages/MessageService.interface';
-import { PROMPT_SERVICE_ID } from './application/interfaces/prompts/PromptService.interface';
-import { SUMMARY_SERVICE_ID } from './application/interfaces/summaries/SummaryService.interface';
+import {
+  MESSAGE_SERVICE_ID,
+  type MessageService,
+} from './application/interfaces/messages/MessageService.interface';
+import {
+  PROMPT_SERVICE_ID,
+  type PromptService,
+} from './application/interfaces/prompts/PromptService.interface';
+import {
+  SUMMARY_SERVICE_ID,
+  type SummaryService,
+} from './application/interfaces/summaries/SummaryService.interface';
 import { AdminServiceImpl } from './application/use-cases/admin/AdminServiceImpl';
 import { ChatGPTService } from './application/use-cases/ai/ChatGPTService';
-import { ChatMemoryManager } from './application/use-cases/chat/ChatMemory';
+import { ChatMemoryManager as ChatMemoryManagerImpl } from './application/use-cases/chat/ChatMemory';
 import { DefaultChatApprovalService } from './application/use-cases/chat/DefaultChatApprovalService';
 import { DefaultChatResetService } from './application/use-cases/chat/DefaultChatResetService';
 import { DefaultChatResponder } from './application/use-cases/chat/DefaultChatResponder';
@@ -54,17 +93,42 @@ import { RepositoryMessageService } from './application/use-cases/messages/Repos
 import { FilePromptService } from './application/use-cases/prompts/FilePromptService';
 import { RepositorySummaryService } from './application/use-cases/summaries/RepositorySummaryService';
 import { TelegramBot } from './bot/TelegramBot';
-import { ACCESS_KEY_REPOSITORY_ID } from './domain/repositories/AccessKeyRepository.interface';
-import { CHAT_ACCESS_REPOSITORY_ID } from './domain/repositories/ChatAccessRepository.interface';
-import { CHAT_CONFIG_REPOSITORY_ID } from './domain/repositories/ChatConfigRepository.interface';
-import { CHAT_REPOSITORY_ID } from './domain/repositories/ChatRepository.interface';
-import { CHAT_USER_REPOSITORY_ID } from './domain/repositories/ChatUserRepository.interface';
-import { MESSAGE_REPOSITORY_ID } from './domain/repositories/MessageRepository.interface';
-import { SUMMARY_REPOSITORY_ID } from './domain/repositories/SummaryRepository.interface';
-import { USER_REPOSITORY_ID } from './domain/repositories/UserRepository.interface';
+import {
+  ACCESS_KEY_REPOSITORY_ID,
+  type AccessKeyRepository,
+} from './domain/repositories/AccessKeyRepository.interface';
+import {
+  CHAT_ACCESS_REPOSITORY_ID,
+  type ChatAccessRepository,
+} from './domain/repositories/ChatAccessRepository.interface';
+import {
+  CHAT_CONFIG_REPOSITORY_ID,
+  type ChatConfigRepository,
+} from './domain/repositories/ChatConfigRepository.interface';
+import {
+  CHAT_REPOSITORY_ID,
+  type ChatRepository,
+} from './domain/repositories/ChatRepository.interface';
+import {
+  CHAT_USER_REPOSITORY_ID,
+  type ChatUserRepository,
+} from './domain/repositories/ChatUserRepository.interface';
+import {
+  MESSAGE_REPOSITORY_ID,
+  type MessageRepository,
+} from './domain/repositories/MessageRepository.interface';
+import {
+  SUMMARY_REPOSITORY_ID,
+  type SummaryRepository,
+} from './domain/repositories/SummaryRepository.interface';
+import {
+  USER_REPOSITORY_ID,
+  type UserRepository,
+} from './domain/repositories/UserRepository.interface';
 import { PinoLoggerFactory } from './infrastructure/logging/PinoLoggerFactory';
 import {
   DB_PROVIDER_ID,
+  type DbProvider,
   SQLiteDbProviderImpl,
 } from './infrastructure/repositories/DbProvider';
 import { SQLiteAccessKeyRepository } from './infrastructure/repositories/SQLiteAccessKeyRepository';
@@ -86,34 +150,40 @@ container
   .to(EnvServiceImpl)
   .inSingletonScope();
 
-container.bind(LOGGER_FACTORY_ID).to(PinoLoggerFactory).inSingletonScope();
-
-container.bind(PROMPT_SERVICE_ID).to(FilePromptService).inSingletonScope();
-
-container.bind(AI_SERVICE_ID).to(ChatGPTService).inSingletonScope();
+container
+  .bind<LoggerFactory>(LOGGER_FACTORY_ID)
+  .to(PinoLoggerFactory)
+  .inSingletonScope();
 
 container
-  .bind(MESSAGE_SERVICE_ID)
+  .bind<PromptService>(PROMPT_SERVICE_ID)
+  .to(FilePromptService)
+  .inSingletonScope();
+
+container.bind<AIService>(AI_SERVICE_ID).to(ChatGPTService).inSingletonScope();
+
+container
+  .bind<MessageService>(MESSAGE_SERVICE_ID)
   .to(RepositoryMessageService)
   .inSingletonScope();
 container
-  .bind(INTEREST_MESSAGE_STORE_ID)
+  .bind<InterestMessageStore>(INTEREST_MESSAGE_STORE_ID)
   .to(InMemoryInterestMessageStore)
   .inSingletonScope();
 container
-  .bind(SUMMARY_SERVICE_ID)
+  .bind<SummaryService>(SUMMARY_SERVICE_ID)
   .to(RepositorySummaryService)
   .inSingletonScope();
 container
-  .bind(HISTORY_SUMMARIZER_ID)
+  .bind<HistorySummarizer>(HISTORY_SUMMARIZER_ID)
   .to(DefaultHistorySummarizer)
   .inSingletonScope();
 container
-  .bind(CHAT_RESET_SERVICE_ID)
+  .bind<ChatResetService>(CHAT_RESET_SERVICE_ID)
   .to(DefaultChatResetService)
   .inSingletonScope();
 container
-  .bind(CHAT_APPROVAL_SERVICE_ID)
+  .bind<ChatApprovalService>(CHAT_APPROVAL_SERVICE_ID)
   .to(DefaultChatApprovalService)
   .inSingletonScope();
 
@@ -123,45 +193,60 @@ container
   .inSingletonScope();
 
 container
-  .bind(INTEREST_CHECKER_ID)
+  .bind<InterestChecker>(INTEREST_CHECKER_ID)
   .to(DefaultInterestChecker)
   .inSingletonScope();
 
-container.bind(ADMIN_SERVICE_ID).to(AdminServiceImpl).inSingletonScope();
-
-container.bind(DB_PROVIDER_ID).to(SQLiteDbProviderImpl).inSingletonScope();
-container.bind(CHAT_REPOSITORY_ID).to(SQLiteChatRepository).inSingletonScope();
 container
-  .bind(CHAT_USER_REPOSITORY_ID)
+  .bind<AdminService>(ADMIN_SERVICE_ID)
+  .to(AdminServiceImpl)
+  .inSingletonScope();
+
+container
+  .bind<DbProvider>(DB_PROVIDER_ID)
+  .to(SQLiteDbProviderImpl)
+  .inSingletonScope();
+container
+  .bind<ChatRepository>(CHAT_REPOSITORY_ID)
+  .to(SQLiteChatRepository)
+  .inSingletonScope();
+container
+  .bind<ChatUserRepository>(CHAT_USER_REPOSITORY_ID)
   .to(SQLiteChatUserRepository)
   .inSingletonScope();
-container.bind(USER_REPOSITORY_ID).to(SQLiteUserRepository).inSingletonScope();
 container
-  .bind(MESSAGE_REPOSITORY_ID)
+  .bind<UserRepository>(USER_REPOSITORY_ID)
+  .to(SQLiteUserRepository)
+  .inSingletonScope();
+container
+  .bind<MessageRepository>(MESSAGE_REPOSITORY_ID)
   .to(SQLiteMessageRepository)
   .inSingletonScope();
 container
-  .bind(SUMMARY_REPOSITORY_ID)
+  .bind<SummaryRepository>(SUMMARY_REPOSITORY_ID)
   .to(SQLiteSummaryRepository)
   .inSingletonScope();
 container
-  .bind(ACCESS_KEY_REPOSITORY_ID)
+  .bind<AccessKeyRepository>(ACCESS_KEY_REPOSITORY_ID)
   .to(SQLiteAccessKeyRepository)
   .inSingletonScope();
 container
-  .bind(CHAT_ACCESS_REPOSITORY_ID)
+  .bind<ChatAccessRepository>(CHAT_ACCESS_REPOSITORY_ID)
   .to(SQLiteChatAccessRepository)
   .inSingletonScope();
 
 container
-  .bind(CHAT_CONFIG_REPOSITORY_ID)
+  .bind<ChatConfigRepository>(CHAT_CONFIG_REPOSITORY_ID)
   .to(SQLiteChatConfigRepository)
   .inSingletonScope();
 
-container.bind(CHAT_MEMORY_MANAGER_ID).to(ChatMemoryManager).inSingletonScope();
+container
+  .bind<ChatMemoryManagerInterface>(CHAT_MEMORY_MANAGER_ID)
+  .to(ChatMemoryManagerImpl)
+  .inSingletonScope();
 
 container
-  .bind(DIALOGUE_MANAGER_ID)
+  .bind<DialogueManager>(DIALOGUE_MANAGER_ID)
   .to(DefaultDialogueManager)
   .inSingletonScope();
 
